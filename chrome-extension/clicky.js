1ec16d32ca49600f2c36e44f1135ccc23a4d1bc5
Jack McCormick <johnmccormick1118@gmail.com>
2019-09-27 14:30:31 -0500

    Adjusted content script.

--- chrome-extension/clicky.js
+++ chrome-extension/clicky.js
@@ -1,13 +1,34 @@
 /* eslint-disable no-undef */
+window.addEventListener("load", function (event) {
+  console.log('Hi world!')
+  const targetNode = document.querySelector('div.pick > div.playerFName');
 
-const clicky = () => {
+  //mutation observer
+  const config = { attributes: true, childList: true, subtree: true };
+  const callback = () => {
+    const firstName = document.querySelector('div.pick > div.playerFName').textContent;
+    const lastName = document.querySelector('div.pick > div.playerLName').textContent;
+    const position = document.querySelector('div.pick > div.playerPos').textContent;
+    const draftingPickString = document.querySelector('#draftBoardHdrInfoContainer > div.draftBoardHdrActive > div.currentPickContainer.infoContainer > div.currentPickHeader.infoHeader > div > span.currentPickNumLabel').textContent;
+    draftingPickInt = parseInt(draftingPickString.replace('Pick: ', ''))
 
-}
+    console.log(firstName, lastName, draftingPickInt)
 
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    if (request.message === "test") {
-      console.log(request)
+    if (lastName !== 'None') {
+      chrome.runtime.sendMessage(
+        {
+          "data": {
+            "pick": draftingPickInt,
+            "isSkater": position === 'G' ? false : true,
+            "name": `${firstName} ${lastName}`
+          }
+        }
+      )
     }
   }
-)
+
+
+  const observer = new MutationObserver(callback)
+
+  observer.observe(targetNode, config)
+});
